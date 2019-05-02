d5d6eb4f40d174094c040f6ff0a97617a10e14fe
Faraz <farazshafiq99+git@gmail.com>
2019-05-02 10:15:11 +1000

    Add main

---
+++ test.js
@@ -0,0 +1,15 @@
+const {deepEqual} = require('assert');
+const words = require('./index.js');
+
+try {
+    deepEqual(words('Hello how are you'), ['Hello', 'how', 'are', 'you']);
+    deepEqual(words(''), []);
+    deepEqual(words('Good'), ['Good']);
+    deepEqual(words('Hello, I am good.'), ['Hello', 'I', 'am', 'good']);
+    deepEqual(words("Tests, now with 'apostrophes'"), ['Tests', 'now', 'with', 'apostrophes']);
+    deepEqual(words('    Birds      chirping ,   in the   MOrning!*@&@and172I/.<>?<love`"it'), ['Birds', 'chirping', 'in', 'the', 'MOrning', 'and', 'I', 'love', 'it']);
+    deepEqual(words("He didn't pay for his burger m'aam"), ['He', "didn't", 'pay', 'for', 'his', 'burger', "m'aam"]);
+    console.log('All tested passed');
+} catch(error) {
+    console.log("Test", error.message, "failed");
+}
